d2c98d6e37a2ab93596418193c4f15cff2314d37
Antkeo1 <sleav143@gmail.com>
2019-03-29 16:12:56 -0400

    Updates

---
+++ precache-manifest.b0ef63e0f5103139065453d912d66cc8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0744302ffbaf85b899b6",
+    "url": "/Songs/static/js/runtime~main.0744302f.js"
+  },
+  {
+    "revision": "b44248b66048219580b9",
+    "url": "/Songs/static/js/main.b44248b6.chunk.js"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/Songs/static/js/2.2315f74c.chunk.js"
+  },
+  {
+    "revision": "a014f9897573d2b509d2e6624dbe9d84",
+    "url": "/Songs/index.html"
+  }
+];
