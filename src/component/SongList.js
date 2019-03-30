cc0a352a05a7e340adc1b543c90b43044bce8760
Antkeo1 <sleav143@gmail.com>
2019-03-30 14:25:20 -0400

    import selectsong from actioncreator to update state

--- src/component/SongList.js
+++ src/component/SongList.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { selectSong } from '../actions'
 
 class SongList extends Component {
   renderList() {
     return this.props.songs.map((song) => {
       return (
-        <div className='iten' key={song.title} >
-          <div className=' right floated content'>
-            <button className='ui button primary'>
+        <div className='item' key={song.title} >
+          <div className='right floated content'>
+            <button className='ui button primary'
+                onClick={() => this.props.selectSong(song)}
+            >
               Select
             </button>
           </div>
@@ -20,13 +23,16 @@ class SongList extends Component {
   }
 
   render() {
-    return <div>{this.renderList()}</div>
+
+    return <div className='ui divided list' >{this.renderList()}</div>
   }
 }
 
 const mapStateToProps = state => {
-
+  console.log(state)
   return { songs: state.songs }
 }
 
-export default connect(mapStateToProps)(SongList)
+export default connect(mapStateToProps, {
+  selectSong
+})(SongList)
